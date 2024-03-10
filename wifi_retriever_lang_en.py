b4e5af1cd246ef9cdb4821f10db891f2a9e80139
Marcus <101027671+mlihk@users.noreply.github.com>
2024-03-10 20:09:08 +0000

    Add files via upload
    Uploaded language files for translation purpose

---
+++ wifi_retriever_lang_en.py
@@ -0,0 +1,17 @@
+#English language file
+translations = {
+    "error_wifi_password":"Error: Unable to retrieve WiFi password.",
+    "email_sent_successfully":"Email sent successfully.",
+    "share_via_email":"Share Via Email",
+    "enter_email_address":"Enter Email address",
+    "send_email":"Send Email",
+    "show_password":"Show Password",
+    "hide_password":"Hide Password",
+    "wifi_password_retriever_title":"WiFi Password Retriever",
+    "connected_wifi_ssid_text":"Connected WiFi SSID: ",
+    "connected_wifi_password_text":"WiFi Password: ",
+    "generate_qrcode_text":"Share via Qr Code",
+    "wifi_not_found_1":"Password for WiFi SSID '",
+    "wifi_not_found_2":"' not found or unable to retrieve",
+    "wifi_not_connected":"No WiFi network currently connected.",
+    }
